99d0c1bcfb287a66870d491ad6e80fcd2e293190
progyu <rocket7101@gmail.com>
2019-06-20 16:11:24 +0900

    Change state area, change method name ...etc

--- todo-v4/src/app/todos/todos.component.ts
+++ todo-v4/src/app/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Todos } from '../todos.interface';
+import { Tabs } from '../tabs.interface';
 
 @Component({
   selector: 'app-todos',
@@ -7,55 +8,66 @@ import { Todos } from '../todos.interface';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
-  navState: string = 'all';
 
-  todos: Todos[] = [
+  private _todos: Todos[] = [
     { id: 1, content: 'HTML', completed: true },
     { id: 2, content: 'CSS', completed: true },
-    { id: 3, content: 'JS', completed: false },
+    { id: 3, content: 'JS', completed: false }
   ]
+
+  _tabs: Tabs[] = [
+    { state: 'All' },
+    { state: 'Active' },
+    { state: 'Completed' }
+  ]
+
+  _navState: string = 'All';
 
   addTodo(input: HTMLInputElement) {
     if (!input.value.trim()) return;
-    this.todos = [{ id: this.generateId(), content: input.value, completed: false }, ...this.todos];
+    this._todos = [{ id: this.generateId, content: input.value, completed: false }, ...this._todos];
     input.value = '';
   }
 
   removeTodo(delId: number) {
-    this.todos = this.todos.filter(({ id }) => delId !== id);
+    this._todos = this._todos.filter(({ id }) => delId !== id);
   }
 
   checkTodo(checkId: number) {
-    this.todos = this.todos.map(todo => checkId === todo.id ? { ...todo, completed: !todo.completed } : todo);
+    this._todos = this._todos.map(todo => checkId === todo.id ? { ...todo, completed: !todo.completed } : todo);
   }
 
-  allCheck(check: boolean) {
-    this.todos = this.todos.map(todo => check ? { ...todo, completed: true } : { ...todo, completed: false });
+  checkAll(check: boolean) {
+    this._todos = this._todos.map(todo => check ? { ...todo, completed: true } : { ...todo, completed: false });
   }
 
   clearCompleted() {
-    this.todos = this.todos.filter(({ completed }) => !completed);
+    this._todos = this._todos.filter(({ completed }) => !completed);
   }
 
-  tabTodo(id: string) {
-    this.navState = id;
-  }
-
-  generateId() {
-    return this.todos.length ? Math.max(...this.todos.map(({ id }) => id)) + 1 : 1;
+  changeState(state: string) {
+    this._navState = state;
   }
 
   get Todo() {
-    if (this.navState === 'all') return this.todos;
-    else if (this.navState === 'active') return this.todos.filter(todo => !todo.completed);
-    else return this.todos.filter(todo => todo.completed);
+    if (this._navState === 'All') return this._todos;
+    else if (this._navState === 'Active') return this._todos.filter(todo => !todo.completed);
+    else return this._todos.filter(todo => todo.completed);
+  }
+
+  get generateId() {
+    return this._todos.length ? Math.max(...this._todos.map(({ id }) => id)) + 1 : 1;
   }
 
   get CompletedCount() {
-    return this.todos.filter(({ completed }) => completed).length;
+    return this._todos.filter(({ completed }) => completed).length;
   }
 
   get LeftCount() {
-    return this.todos.length - this.CompletedCount;
+    return this._todos.length - this.CompletedCount;
+  }
+
+  get checkAllMark() {
+    return this._todos.every(({ completed }) => completed);
   }
 }
